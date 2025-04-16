2257b02e77608174ea16ccb30a17fc9d8a6054ae
talom45 <164904681+talom45@users.noreply.github.com>
2025-04-16 15:43:27 +0200

    made changes

--- Tracker_Dashboard/static/js/script.js
+++ Tracker_Dashboard/static/js/script.js
@@ -2,4 +2,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("JavaScript is loaded!");
     // Add your custom JavaScript logic here
+
+    // JavaScript to toggle supervisor view
+    const roleSelector = document.getElementById('role-selector');
+    const studentNav = document.getElementById('student-nav');
+    const supervisorNav = document.getElementById('supervisor-nav');
+
+    roleSelector.addEventListener('change', () => {
+        if (roleSelector.value === 'supervisor') {
+            studentNav.classList.add('hidden');
+            supervisorNav.classList.remove('hidden');
+        } else {
+            supervisorNav.classList.add('hidden');
+            studentNav.classList.remove('hidden');
+        }
+    });
 });
